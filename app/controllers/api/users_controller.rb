212ff4b229e71261f4b8f7d7e8e8e81672cb25d7
Ian Nguyen <iannknguyen@gmail.com>
2016-01-29 20:31:16 -0800

    remove user

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,9 +1,9 @@
 class Api::UsersController < ApplicationController
-  before_action :authenticate_user!
-  def current
-    user = User.includes(:skins).includes(:payouts => :skins).find(current_user.id)
-    render json: user
-  end
+  # before_action :authenticate_user!
+  # def current
+  #   user = User.includes(:skins).includes(:payouts => :skins).find(current_user.id)
+  #   render json: user
+  # end
   def index
     user = User.includes(:skins).includes(:payouts => :skins).find(current_user.id)
     render json: user
