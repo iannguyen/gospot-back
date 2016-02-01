23336a736ee02c3352b83cb39c322412db28ab6a
Ian Nguyen <iannknguyen@gmail.com>
2016-02-01 01:45:25 -0800

    minor

--- app/controllers/api/bets_controller.rb
+++ app/controllers/api/bets_controller.rb
@@ -11,9 +11,10 @@ class Api::BetsController < ApplicationController
     skin_ids = bet_params[:skin_ids].map(&:to_i)
     skins = Skin.where(id: skin_ids).all
     sum = skins.sum(:price)
+    match = Match.find(params[:match_id])
     @bet = Bet.new(bet_params.except('skins'))
     @bet.skins = skins
-    if sum >= 10 && @bet.save
+    if sum >= 10 && match.open && @bet.save
       render json: @bet, status: 201
     elsif sum < 10
       render json: { failures: 'Minimum bet $10' }, status: 422
