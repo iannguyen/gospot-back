70063d3f84d58a7f1ec5c8e6d41a79a8f68fea05
Ian Nguyen <iannknguyen@gmail.com>
2016-02-04 21:15:42 -0800

    add basic_match serializer

--- app/controllers/api/matches_controller.rb
+++ app/controllers/api/matches_controller.rb
@@ -1,10 +1,11 @@
 class Api::MatchesController < ApplicationController
   def index
-    render json: Match.includes(bets: [:skins, :user]).all
+    @matches = Match.includes(bets: [:skins, :user]).all
+    render json: @matches, each_serializer: Api::BasicMatchSerializer
   end
 
   def show
     @match = Match.includes(bets: [:skins, :user]).find(params[:id])
-    render json: @match
+    render json: @match, serializer: Api::MatchSerializer
   end
 end
